a80bf4f9fd21633c3c4f138223216a58629a0fb4
asfaltouklad4ik <asfaltouklad4ik@gmail.com>
2014-04-09 15:56:56 +0300

    change gemfile for heroku

---
+++ vendor/bundle/ruby/2.1.0/gems/nokogiri-1.6.1/ports/i686-linux-gnu/libxslt/1.1.26/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/home/user/organizer/vendor/bundle/ruby/2.1.0/gems/nokogiri-1.6.1/ports/i686-linux-gnu/libxslt/1.1.26/lib"
+XSLT_LIBS="-lxslt  -L/home/user/organizer/vendor/bundle/ruby/2.1.0/gems/nokogiri-1.6.1/ports/i686-linux-gnu/libxml2/2.8.0/lib -lxml2 -lz -lm -lm"
+XSLT_INCLUDEDIR="-I/home/user/organizer/vendor/bundle/ruby/2.1.0/gems/nokogiri-1.6.1/ports/i686-linux-gnu/libxslt/1.1.26/include"
+MODULE_VERSION="xslt-1.1.26"
